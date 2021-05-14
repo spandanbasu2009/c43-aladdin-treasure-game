c6168f9d3f5c48e7ba5669e8e8437811f66cdcdd
spandanbasu2009 <74770709+spandanbasu2009@users.noreply.github.com>
2021-05-14 11:35:53 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white");
+    textSize(15);
+    text("R E V B A I L A", 100,50);
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 100,70);
+// add code for displaying rest of the names and the hints.
+    fill("white");
+    textSize(15);
+    text("C U T N I F O N",500,100);
+    fill("lightblue");
+    text("Hint: What comes before draw,preload and setup ??",500,120);
+    fill("white");
+    textSize(15);
+    text("A T A D A S B A",300,300);
+    fill("lightblue");
+    text("Hint: When we want to write code in a project in firebase,we go to realtime ___________!!",300,320);
+}
